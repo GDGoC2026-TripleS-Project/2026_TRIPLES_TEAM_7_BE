090ee065b978fa01b9cbf96d2340e8faf052fd6a
jjy12163 <lalalove0310@gmail.com>
2026-02-19 22:00:10 +0900

    Feat: deadlineAt 기준 정렬 기능 추가

--- src/controllers/canvasController.js
+++ src/controllers/canvasController.js
@@ -1,4 +1,5 @@
 const canvasService = require('../services/canvasService');
+const job_cards = require('../models/job_cards/job_cards');
 
 exports.getCanvasItems = async (req, res) => {
   try {
@@ -32,46 +33,42 @@ exports.setCanvasItems = async (req, res) => {
   }
 };
 
-// exports.getSortedCanvasItems = async (req, res) => {
-//   try {
-//     const userId = req.user.id;
-//     const sort = req.query.sort;
-//     let sortedCanvasItems;
+exports.getSortedCanvasItems = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const sort = req.query.sort;
+    let sortedCanvasItems;
 
-//     const result = User.findByPk(userId, {
-//             // 요청하신 3가지 정보만 선택하여 리턴
-//             attributes: ['address', 'resumeUrl']
-//         });
-//         if(job_cards.findByUserId(userId) === null) {
-//           return res.status(204).json({
-//             success: true,
-//             message: '필터링 할 카드가 없습니다.',
-//             data: []
-//           });
-//         }
+    if (!job_cards.findAll({ where: { userId: userId }})) {
+      return res.status(500).json({ 
+        success: false,
+        message: '조회할 카드가 없습니다.',
+        error: error.message
+      })
+    }
 
-//     switch (sort) {
-//       case 'deadline':
-//          sortedCanvasItems = await canvasService.getSortedCanvasItemsbyDeadline(userId);
-//         break;
-//       case 'salary':
-//         sortedCanvasItems = await canvasService.getSortedCanvasItemsbySalary(userId);
-//         break;
-//       case 'distance':
-//         sortedCanvasItems = await canvasService.getSortedCanvasItemsbyDistance(userId);
-//         break;
-//       case 'matchedPercent':
-//         sortedCanvasItems = await canvasService.getSortedCanvasItemsbyMatchedPercent(userId);
-//         break;
-//     }       
+    switch (sort) {
+      case 'deadline':
+         sortedCanvasItems = await canvasService.getSortedCanvasItemsbyDeadline(userId);
+        break;
+      case 'salary':
+        sortedCanvasItems = await canvasService.getSortedCanvasItemsbySalary(userId);
+        break;
+      case 'distance':
+        sortedCanvasItems = await canvasService.getSortedCanvasItemsbyDistance(userId);
+        break;
+      case 'matchedPercent':
+        sortedCanvasItems = await canvasService.getSortedCanvasItemsbyMatchedPercent(userId);
+        break;
+    }       
 
-//     res.status(200).json(sortedCanvasItems);
+    res.status(200).json(sortedCanvasItems);
 
-//   } catch (error) {
-//     res.status(500).json({
-//       success: false,
-//       sort: req.query.sort,
-//       error: error.message
-//     });
-//   }
-// };
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      sort: req.query.sort,
+      error: error.message
+    });
+  }
+};
