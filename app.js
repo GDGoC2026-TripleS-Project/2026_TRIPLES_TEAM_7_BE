0bccbcf56a1de8ffc653fbd32834c96f52aa028e
jjy12163 <lalalove0310@gmail.com>
2026-02-20 23:38:51 +0900

    Feat: db 컬럼 수정 반영

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ try {
 const app = express();
 app.set('port', process.env.PORT || 8080);
 
-sequelize.sync({ force: false })
+sequelize.sync({ alter: true })
   .then(() => {
     console.log('데이터베이스 연결 성공');
   })
